79a861ec6fd3ab2f46395af5547ce9eaa3e186b0
MiniCash <tminecraft2143@gmail.com>
2026-01-30 21:33:16 +0900

    Add input handling and error management in t3.py

---
+++ t3.py
@@ -0,0 +1,13 @@
+try:
+    args1 = input("引数1つ目を入力してください")
+    args2 = input("引数２つ目を入力してください")
+
+    result = int(args1) + int(args2)
+    print(f"結果：{result}")
+except ValueError:   
+    print("エラー: 有効な数字を入力してください。")
+except Exception as e:
+    print("エラーが発生しました")
+finally:
+    print("処理を終了しました")
+
